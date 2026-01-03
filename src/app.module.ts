7c91505e14c7d98a1d8af913dc90d51e28fd5c01
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-03 14:40:15 -0600

    Import img module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
+import { ImgModule } from '@img/img.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, ImgModule],
 })
 export class AppModule {}
