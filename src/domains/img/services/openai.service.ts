aba634decfeba72fd2dabc51e392747179234b7c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-03 15:22:14 -0600

    Edit image

--- src/domains/img/services/openai.service.ts
+++ src/domains/img/services/openai.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { EnvService } from '@config/env';
-import openAIClient from 'openai';
+import openAIClient, { toFile } from 'openai';
 import { base64toImage } from '@img/util/base64toImage.util';
 
 @Injectable()
@@ -24,6 +24,27 @@ export class OpenAIService {
 
     if (!res.data?.[0]?.b64_json)
       throw new Error('Image generation failed: No data returned from OpenAI');
+
+    return base64toImage(res.data[0].b64_json);
+  }
+
+  async editImage(image: Express.Multer.File, prompt: string) {
+    const filename = image.originalname ?? 'image.png';
+    const mimeType = image.mimetype ?? 'image/png';
+
+    const cImage = await toFile(image.buffer, filename, { type: mimeType });
+
+    const res = await this.openAI.images.edit({
+      model: 'gpt-image-1-mini',
+      image: [cImage],
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      quality: 'low',
+    });
+
+    if (!res.data?.[0]?.b64_json)
+      throw new Error('Image editing failed: No data returned from OpenAI');
 
     return base64toImage(res.data[0].b64_json);
   }
