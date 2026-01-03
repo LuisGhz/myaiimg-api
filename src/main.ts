def90338254152e71d36f553384d14d9f37a335a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-03 08:33:23 -0600

    Add configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { EnvService } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const envService = app.get(EnvService);
+  const logger = new Logger('Main');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors();
+  app.use(cookieParser());
+  await app.listen(envService.port);
+  logger.log(`Application is running on port ${envService.port}`);
 }
 bootstrap();
