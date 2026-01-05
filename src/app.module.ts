8a10f33c2dbb0d2f969c5d76f17fafbfad96c94c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-05 14:30:25 -0600

    Implement strategy and guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AppConfigModule } from './config/app-config.module';
 import { ImgModule } from '@img/img.module';
+import { JwtStrategy } from '@core/strategies/jwt.strategy';
+import { AppAuthGuard } from '@core/guards/app-auth.guard';
 
 @Module({
-  imports: [AppConfigModule, ImgModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    AppConfigModule,
+    ImgModule,
+  ],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: AppAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
